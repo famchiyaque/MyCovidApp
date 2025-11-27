cbfce34febd1a60a5b5099047ba27174c39c2ff3
Andre Agle <andrezala03@gmail.com>
2025-11-27 16:39:07 -0600

    add first usecase, get covid by date

--- app/src/main/java/com/app/mycovidapp/domain/usecase/GetCovidByDateUseCase.kt
+++ app/src/main/java/com/app/mycovidapp/domain/usecase/GetCovidByDateUseCase.kt
@@ -1,4 +1,27 @@
 package com.app.mycovidapp.domain.usecase
 
-class GetCovidByDateUseCase {
+import com.app.mycovidapp.domain.common.Result
+import com.app.mycovidapp.domain.repository.CovidRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetCovidByDateUseCase
+    @Inject
+    constructor(
+        private val repository: CovidRepository
+    ){
+    operator fun invoke(
+        appointmentName: String,
+        date: String,
+    ): Flow<Result<List<String>>> =
+        flow {
+            try {
+                emit(Result.Loading)
+                val data = repository.getDateAvailability(appointmentName, date)
+                emit(Result.Success(data))
+            } catch (e: Exception) {
+                emit(Result.Error(e))
+            }
+        }
 }
